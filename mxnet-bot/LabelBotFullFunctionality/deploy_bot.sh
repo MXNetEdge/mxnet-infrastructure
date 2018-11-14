a9d64acafee8e0e9f769329b99f8e34e73d2248c
Harsh Patel <harshpatel081296@gmail.com>
2018-11-13 19:57:40 -0800

    Includes LabelBot Cross Account Log Functionality

--- mxnet-bot/LabelBotFullFunctionality/deploy_bot.sh
+++ mxnet-bot/LabelBotFullFunctionality/deploy_bot.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Script to deploy the label bot to our dev account
 set -e
-
 export AWS_PROFILE=mxnet-ci-dev
+
+dest_account="106780517423"
+bot_account="968277166688"
+
+sed "s/###HERE###/$dest_account/g" log-access-trust-policy.json-template > log-access-trust-policy.json
+sed "s/###HERE###/$bot_account/g" log-access-policy.json-template > log-access-policy.json
+
+aws iam create-role --role-name LabelBotLogAccessRole --assume-role-policy-document file://log-access-trust-policy.json
+aws iam create-policy --policy-name LabelBotLogAccessPolicy --policy-document file://log-access-policy.json
+aws iam attach-role-policy --policy-arn arn:aws:iam::$bot_account:policy/LabelBotLogAccessPolicy --role-name LabelBotLogAccessRole
+
 sls deploy -v
